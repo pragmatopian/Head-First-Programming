8a9b613a7ce27069af924c073a654ad83dfb5d1c
pragmatopian <pragmatopian@gmail.com>
2011-04-23 11:31:02 +0200

    Update to check price every 15 minutes

--- Ch02_BeanPrices.py
+++ Ch02_BeanPrices.py
@@ -1,9 +1,18 @@
 import urllib.request
- 
-page = urllib.request.urlopen("http://www.beans-r-us.biz/prices-loyalty.html") 
-text = page.read() . decode("utf8")
+import time
 
+price = float(99.99)
 
- 
-price = text[text.find("$")+1:text.find("$")+6]
-print(price)
+while price > 4.74:
+    time.sleep(900)
+    page = urllib.request.urlopen("http://www.beans-r-us.biz/prices.html") 
+    text = page.read() . decode("utf8")
+
+    where = text.find('>$')
+    
+    start_of_price = where + 2
+    end_of_price = start_of_price + 4
+     
+    price = float(text[start_of_price:end_of_price])
+
+print("Buy!")
