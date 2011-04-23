b404e9cacb586dd3bef86c16f020ffb0acd8bf56
pragmatopian <pragmatopian@gmail.com>
2011-04-23 18:02:19 +0200

    Chapter 4 exercise - accessing scores in a text file, putting them in an array and sorting.

---
+++ Ch04_SurfingScores.py
@@ -0,0 +1,16 @@
+scores = []
+
+result_f = open("results.txt")
+
+for line in result_f:
+    (name,score) = line.split()
+    scores.append(float(score))
+
+result_f.close
+
+scores.sort(reverse=True)
+
+print("The top scores were:")
+print(scores[0])
+print(scores[1])
+print(scores[2])
